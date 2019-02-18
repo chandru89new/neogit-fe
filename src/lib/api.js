1abcaf557f72f7aa9890126ea197b5de1331e588
Chandru <chandru89new@gmail.com>
2019-02-18 18:15:15 +0530

    modified backend endpoint

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
 const baseURL = "https://api.github.com"
-const tokenAPI = (process.env.NODE_ENV == 'production') ? 'http://123.456.789:3000' : 'http://localhost:3000'
+const tokenAPI = (process.env.NODE_ENV == 'production') ? 'http://18.202.198.215:3000/' : 'http://localhost:3000'
 export default {
   user: `${baseURL}/users/`,
   repo: `${baseURL}/repos/`,
